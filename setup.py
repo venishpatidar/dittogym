a9c0385e8fe70fe3adb3568145b52193e4ff5e6b
Venish Patidar <61043425+venishpatidar@users.noreply.github.com>
2024-11-21 12:50:36 -0700

    Update setup.py to include package data as well

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ setup(
     version='0.1',
     packages=['dittogym'],
     install_requires=['opencv-python', 'gym', 'numpy', 'taichi'],
+    include_package_data=True,
+    package_data={
+        'dittogym': ['bg/*'],  # Include specific directories
+    },
 )
